f1e0b289a73399ba0680e490d81540390633cce5
kmcnama23 <kmcnama23@gmail.com>
2018-11-17 15:35:12 -0500

    Delete Dynamic_Watchman.py

--- Dynamic_Watchman.py
+++
@@ -1,8 +0,0 @@
-import xlsxwriter
-
-ad_review_week = input("Which ad week are you reviewing? ")
-Gatekeeper = xlsxwriter.Workbook("C:/Users/Kyle McNamara/Desktop/Work/Code/Gatekeeper/Gatekeeper_Wk"+ad_review_week+"_Working_File.xlsx") 
-ws1 = Gatekeeper.add_worksheet()	
-
-Gatekeeper.close()	
-
