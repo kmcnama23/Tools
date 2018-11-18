3202c4105c53207a75e459a38ed81e1f49498df2
kmcnama23 <kmcnama23@gmail.com>
2018-11-17 23:45:55 -0500

    Delete installed_library_check.py

--- installed_library_check.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import sys
-
-reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
-installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
-
-print(installed_packages)
-
