542c6a2f984deadcc74ab030315fb2bc5cb7a680
kmcnama23 <kmcnama23@gmail.com>
2018-11-18 23:36:05 -0500

    Add files via upload

--- Elegant_Watchman.py
+++ Elegant_Watchman.py
@@ -1,23 +1,15 @@
-import openpyxl
+import win32com.client as win32
 
 ad_review_week = input("Which ad week are you reviewing?(one last time) ")	
 destination_path='C:\\Users\\Kyle McNamara\\Desktop\\Work\\Code\\Gatekeeper\\Gatekeeper_Wk'+ad_review_week+'_Working_File.xlsx'
 
-wb = openpyxl.load_workbook(destination_path)
-ws1 = wb['GM Wk'+ad_review_week]
-row_count = ws1.max_row
+xl = win32.gencache.EnsureDispatch('Excel.Application')
+wb = xl.Workbooks.Open('C:\\Users\\Kyle McNamara\\Desktop\\Work\\Code\\Gatekeeper\\Gatekeeper_Wk'+ad_review_week+'_Working_File.xlsx')
+ws = wb.Worksheets('GM Wk'+ad_review_week)
+xl.Visible=True
+wb.Close(savechanges=1)
+xl.Quit()
 
-row=2
-while row < row_count:
-		for row,formula in enumerate(list(ws1.columns)[1],1):
-			concat= '=G%d&"|"&N%d&"|"&Q%d' % (row,row,row)
-			formula.value = concat
-			row += 1
-			
 
-ws1['B1']="History Key"
 
-		
-wb.save(destination_path)
-	
 
